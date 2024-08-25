3e61841c9c5c0bdd046833dd4f85fc8b5305caec
Guram Salia <guramsalia@gmail.com>
2024-08-25 17:17:47 +0400

    add first endpoints: test and for checking if date is a working day, add to builder service singlton implmentation of working day repo

---
+++ Vacation.API/Models/Dtos/DayStatusDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Vacation.API.Models.Dtos
+{
+    public class DayStatusDTO
+    {
+        public DateOnly Date { get; }
+
+        public bool IsWorkingDay { get; }
+
+        public DayStatusDTO(DateOnly date, bool isNonWorkingDay)
+        {
+            Date = date;
+            IsWorkingDay = isNonWorkingDay;
+        }
+
+        public DayStatusDTO(string date, bool isWorkingDay)
+        {
+            if (!DateOnly.TryParse(date, out DateOnly parsedDate))
+            {
+                throw new ArgumentException("invalid date format", nameof(date));
+            }
+            Date = parsedDate;
+            IsWorkingDay = isWorkingDay;
+        }
+    }
+}
--- Vacation.API/Program.cs
+++ Vacation.API/Program.cs
@@ -1,3 +1,6 @@
+using Vacation.API.Repositories.InMemory;
+using Vacation.API.Repositories.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// gs: for dependency injection of an implementation. Delete this comment later.
+builder.Services.AddSingleton<INonWorkingDaysRepository, InMemoryNonWorkingDaysRepository>();
 
 var app = builder.Build();
 
